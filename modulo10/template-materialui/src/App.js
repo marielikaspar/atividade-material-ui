891cc9c2ca21024554bafb40e9daf4014cf6e691
Marieli Kaspar <kaspar.mari@gmail.com>
2024-12-03 16:47:13 -0300

    novo_commit

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <ListarTarefa />
+      <Login />
     </div>
   );
 }
